01a0f30d56839fd4ccb01628bb3c27704b4d066c
Michelle Kiziak <makiziak@gmail.com>
2016-12-14 11:09:34 -0800

    removed css footer line of code

--- content-aside.php
+++ content-aside.php
@@ -1,5 +1,5 @@
 <!-- Aside Post -->
-  <h4>Aside Post:<?php the_title(); ?></h4>
+  <h4>Post:<?php the_title(); ?></h4>
 
       <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
 
--- footer.php
+++ footer.php
@@ -3,9 +3,14 @@
   <footer>
         <p>This is my footer</p>
         <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-  </footer>
+
+
+
+     </footer>
 
   <?php wp_footer(); ?>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
   </body>
 </html>
