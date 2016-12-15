b434fdf3350351095bb8bb732880e0c38ec1b93b
Michelle Kiziak <makiziak@gmail.com>
2016-12-14 16:32:12 -0800

    added entry-deader in content file

--- content.image.php
+++ content.image.php
@@ -1,5 +1,12 @@
-<h3>Image Post: <?php the_title(); ?></h3>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+
+<header class="entry-header">
+    <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() )),'</a></h1>' ); ?>
+    <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, <?php the_category(); ?></small>
+</header>
+
       <div class="thumbnail-img"><?php the_post_thumbnail('medium'); ?></div>
 
+  <p><?php the_content(); ?></p>
 
     <hr>
--- content.php
+++ content.php
@@ -1,7 +1,11 @@
         <!-- do stuff ... -->
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+
   <?php the_title(); ?>
       <div class="thumbnail-img"><?php the_post_thumbnail('medium'); ?></div>
       <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?><?php the_category(); ?></small>
       <p><?php the_content(); ?></p>
 
     <hr>
+
+  </article>
